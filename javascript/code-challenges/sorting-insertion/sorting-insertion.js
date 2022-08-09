95ec9fb2d15a94c2846856bfcb512394d7afd79e
AhmadTayseer <ahmadtayseer@ymail.com>
2022-08-09 22:54:31 +0300

    add sorting-insertion branch

---
+++ javascript/code-challenges/sorting-insertion/sorting-insertion.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function insertionSort(arr) {
+  let temp, j;
+  for (let i = 1; i < arr.length; i++) {
+    temp = arr[i];
+    j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
+}
